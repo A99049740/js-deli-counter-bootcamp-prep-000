6316161128cdf42e57afc77e5f51b031ad310020
jill zhang <talktojillzhang@gmail.com>
2018-09-01 05:44:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function nowServing(line){
     return `Currently serving ${line.shift()}.`
   }
 }
+function currentLine(line){
+  if (line.length==0){
+    return "The line is currently empty"
+  }
+  else{
+    return `The line is currently: ${emunator(line)}`
+  }
+}
+function emunator(line){
+  var toreturn = ""
+  for (var customer of line){
+    toreturn = toreturn+", "+line.indexOf(customer)+". "+customer
+  }
+}
