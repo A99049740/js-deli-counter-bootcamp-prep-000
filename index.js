7bb2f7996e1b49c6d8a51c1378f4328016b79c0e
jill zhang <talktojillzhang@gmail.com>
2018-09-01 05:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function takeANumber(line,customer){
   line.push(customer)
   return `Welcome, ${customer}. You are number ${line.length} in line.`
 }
-function nowServing(){
-  
+function nowServing(line){
+  if (line.length==0){
+    return "there is nobody waiting to be served"
+  }
+  else{
+    return line.pop()
+  }
 }
