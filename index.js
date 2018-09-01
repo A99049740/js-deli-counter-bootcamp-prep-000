87facbd793cf6762c0c482c0704627f768b68f87
jill zhang <talktojillzhang@gmail.com>
2018-09-01 05:30:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function takeANumber(line,customer){
   console.log(line)
   return `Welcome, ${customer}. You are number ${line.findIndex(customer)+1} in line.`
 }
-function checkCustomer(customer){
-  
+function checkCustomer(customer,check){
+  return customer == check
 }
 var line = ['grace','tom']
 console.log(line.findIndex('tom'))
