233c409b95d44b967c3a89c9e686520502a6bbd8
jill zhang <talktojillzhang@gmail.com>
2018-09-01 05:35:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line,customer){
-  console.log(line)
-  return `Welcome, ${customer}. You are number ${line.indexOf(customer)+1} in line.`
+  line.push(customer)
+  return `Welcome, ${customer}. You are number ${line.length} in line.`
 
 }
